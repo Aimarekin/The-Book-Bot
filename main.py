15b0a9940b192af2d814020fae7cc0f817bf2f0b
Aimarekin <38393797+Aimarekin@users.noreply.github.com>
2018-11-18 15:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 import asyncio
 import json
 import traceback
+
 #Following only in Heroku
+import os
 from boto.s3.connection import S3Connection
 s3 = S3Connection(os.environ['DISCORD_KEY'])
 print(s3)
